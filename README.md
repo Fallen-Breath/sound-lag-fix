7ef36858c23ae4a37fb2783853853c5cab5799ff
Fallen_Breath <yqx791125833@gmail.com>
2020-11-02 20:57:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## sound-lag-fix
+
+Fix permanent fps drop causes by sound system in version from 1.14 to 1.16
---
+++ src/main/java/me/fallenbreath/soundlagfix/mixins/SourceManagerMixin.java
@@ -0,0 +1,31 @@
+package me.fallenbreath.soundlagfix.mixins;
+
+import net.minecraft.client.sound.Channel;
+import net.minecraft.client.sound.Source;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Channel.SourceManager.class)
+public class SourceManagerMixin
+{
+	@Shadow private boolean stopped;
+
+	@Shadow private Source source;
+
+	/**
+	 * @author Fallen_Breath
+	 * @reason this.source might be null all the time if Channel#createSource fails
+	 */
+	@Inject(method = "isStopped", at = @At("HEAD"))
+	private void nullSourceIsStoppedStateToo(CallbackInfoReturnable<Boolean> cir)
+	{
+		if (this.source == null)
+		{
+			this.stopped = true;
+		}
+	}
+}
